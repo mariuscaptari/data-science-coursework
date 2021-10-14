dca27f95ac9cb6dd527671d3659e18ac9eae0593
Max Valk <maxvalk@pop-os.localdomain>
2021-10-14 10:43:46 +0200

    Reformatted the files so that PyCharm would stop complaining

--- 4/partA.py
+++ 4/partA.py
@@ -3,47 +3,49 @@
 #
 ###
 
-#from utilities import read_text_file_whole
+# from utilities import read_text_file_whole
 from inverted_index import InvertedIndex
 from semantic_space import SemanticSpace
 
+
 def main():
-  # Read in the sample data we used in class
-  documents = read_data("./dataA")
+    # Read in the sample data we used in class
+    documents = read_data("./dataA")
 
-  # Now we have the data we can create a crude inverted index using
-  # a simple (inefficient!) linked list class.
-  inv_ind = InvertedIndex()
+    # Now we have the data we can create a crude inverted index using
+    # a simple (inefficient!) linked list class.
+    inv_ind = InvertedIndex()
 
-  # Add the documents
-  for d in documents:
-    inv_ind.add_document(d)
+    # Add the documents
+    for d in documents:
+        inv_ind.add_document(d)
 
-  # Create our semantic space and SVD computation
-  ss = SemanticSpace(inv_ind)
+    # Create our semantic space and SVD computation
+    ss = SemanticSpace(inv_ind)
 
-  # Fold in our query vector
-  # TODO:  complete this method
-  q = ss.create_query_vector("Human Computer Interaction")
+    # Fold in our query vector
+    # TODO:  complete this method
+    q = ss.create_query_vector("Human Computer Interaction")
 
-  # This loop prints out the similarity scores between each document
-  # and our query vector - results should match those of class.
-  # TODO:  Complete cosine_with_doc method
-  print("Showing similarities between our query and documents in semantic space")
-  for i in range(9):
-    print(inv_ind.docs[i] + ": " + '{0:0.3f}'.format(ss.cosine_with_doc(q, i)))
+    # This loop prints out the similarity scores between each document
+    # and our query vector - results should match those of class.
+    # TODO:  Complete cosine_with_doc method
+    print("Showing similarities between our query and documents in semantic space")
+    for i in range(9):
+        print(inv_ind.docs[i] + ": " + '{0:0.3f}'.format(ss.cosine_with_doc(q, i)))
 
-  print()
-
-  # This code snippet iterates through every pair of terms in our semantic
-  # space printing out the similarities (cosine) score of each pair.
-  # TODO:  Complete cosine_with_term method
-  print("Showing similarity between all terms with one another")
-  for i in range(12):
-    print("Similarity with " + inv_ind.terms[i][0])
-    for j in range(12):
-      print('{0: >10}'.format(inv_ind.terms[j][0]) + ": " + '{0:0.3f}'.format(ss.cosine_with_term(i, j)))
     print()
+
+    # This code snippet iterates through every pair of terms in our semantic
+    # space printing out the similarities (cosine) score of each pair.
+    # TODO:  Complete cosine_with_term method
+    print("Showing similarity between all terms with one another")
+    for i in range(12):
+        print("Similarity with " + inv_ind.terms[i][0])
+        for j in range(12):
+            print('{0: >10}'.format(inv_ind.terms[j][0]) + ": " + '{0:0.3f}'.format(ss.cosine_with_term(i, j)))
+        print()
+
 
 #####
 #
@@ -63,14 +65,15 @@ def main():
 #           b - corresponds to the text of the document as a string
 ###
 def read_data(folder):
-  import os
+    import os
 
-  documents = [ ]
-  for file in os.listdir(folder):
-    if file.endswith(".txt"):
-      documents.append((file.rsplit('.', 1)[0], (read_text_file_whole(folder + "/" + file))))
+    documents = []
+    for file in os.listdir(folder):
+        if file.endswith(".txt"):
+            documents.append((file.rsplit('.', 1)[0], (read_text_file_whole(folder + "/" + file))))
 
-  return documents
+    return documents
+
 
 ##
 # Reads in an entire text file as a giant string
@@ -84,18 +87,19 @@ def read_data(folder):
 # 07/10/2019 - Created (CJL).
 ###
 def read_text_file_whole(fileName):
-  try:
-    file_obj = open(fileName, 'r')
-    fileContents = file_obj.read()
-  except OSError as ex:
-    print("In IOErrorBlock")
-    print("In method [read_text_file_whole] - " + str(ex))
-    print("Out IOErrorBlock")
-    return ""
+    try:
+        file_obj = open(fileName, 'r')
+        fileContents = file_obj.read()
+    except OSError as ex:
+        print("In IOErrorBlock")
+        print("In method [read_text_file_whole] - " + str(ex))
+        print("Out IOErrorBlock")
+        return ""
 
-  file_obj.close()
+    file_obj.close()
 
-  return fileContents
+    return fileContents
+
 
 if __name__ == "__main__":
-  main()
+    main()
